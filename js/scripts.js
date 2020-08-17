0da1cb10636869da3d7e2591c94209ff6ff7d01b
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-17 13:30:09 -0700

    Add html form and jquery event handler to populate sorted, and capitalized list items in a return div
    Co-authored-by: Jaki Widby <jaki.widby@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    const item1Input = $("#item1").val();
+    const item2Input = $("#item2").val();
+    const item3Input = $("#item3").val();
+    const item4Input = $("#item4").val();
+
+    let list = [item1Input, item2Input, item3Input, item4Input];
+    let sortedList = list.sort();
+    let capitalizedList = [];
+    sortedList.forEach(function (item) {
+      capitalizedList.push(item.toUpperCase());
+    });
+    capitalizedList.forEach(function(item) {
+      $("#result").append("<li>"+item+"</li>");
+    });
+  });
+});
